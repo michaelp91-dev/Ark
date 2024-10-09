aed860b37a3fec6865722db31a33a017f67d04d7
michaelp91-dev <80403994+michaelp91-dev@users.noreply.github.com>
2024-10-09 09:30:28 -0500

    Update ark.c

--- ark.c
+++ ark.c
@@ -10,6 +10,14 @@
 #define I2C_SDA 2
 #define I2C_SCL 3
 
+//mpu state
+uint8_t accel_res; uint16_t accel_res_val;                      // 0=> 16384, 1=>8192, 2=>4096, 3=>2048  
+uint8_t gyro_res;                                               // 0=> 131,   1=>65.5, 2=>32.8, 3=>16.4 
+int16_t accel_x_deviation, accel_y_deviation, accel_z_deviation;// accelerometer standard deviation
+int16_t gyro_x_deviation, gyro_y_deviation, gyro_z_deviation;   // gyroscope standard deviation
+int16_t accel_x_offset, accel_y_offset, accel_z_offset;         // accelerometer offset
+int16_t gyro_x_offset, gyro_y_offset, gyro_z_offset;            // gyroscope offset
+
 // Helpers
 void i2c_write_reg(uint8_t i2c_address, uint8_t reg, uint8_t data)
 {
@@ -51,6 +59,104 @@ static void mpu6050_reset() {
     printf("\r\nMPU6050 Setup Complete\r\n");
 } 
 
+void mpu_set_sample_rate(uint8_t divider)
+{
+    i2c_write_reg(mpu6050_reg.address, mpu6050_reg.config, 0b00000110); // => set DLPF as 1kHz
+
+    switch(divider)
+    {
+        case 1:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b00000001);
+        break;
+
+        case 2:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b00000010);
+        break;
+
+        case 4:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b00000100);
+        break;
+
+        case 8:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b00001000);
+        break;
+
+        case 16:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b00010000);
+        break;
+        
+        case 32:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b00100000);
+        break;
+
+        case 64:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b01000000);
+        break;
+
+        case 128:
+            i2c_write_reg(mpu6050_reg.address, mpu6050_reg.SMPLRT_DIV, 0b10000000);
+        break;
+    }  
+}
+
+void mpu_setresolution(uint8_t gy_res, uint8_t ac_res)
+{
+    uint8_t check, resolution = 0;
+    uint8_t res_index = 0;
+    uint16_t res_value = 0;
+ 
+    //GYROSCOPE RESOLUTION
+    switch(gy_res)
+    {
+        case 0: //+- 250
+            resolution = 0b00000000;
+            res_index = 0;
+            break;
+        case 1: //+- 500
+            resolution = 0b00001000;
+            res_index = 1;
+            break;
+        case 2: //+- 1000
+            resolution = 0b00010000;
+            res_index = 2;
+            break;
+        case 3: //+- 2000
+            resolution = 0b00011000;
+            res_index = 3;
+            break;
+    }
+    i2c_write_reg(mpu6050_reg.address, mpu6050_reg.gyro_res, resolution);
+    gyro_res = res_index;
+
+    //ACCELEROMETER RESOLUTION
+    switch(ac_res)
+    {
+        case 0: //+- 2
+            resolution = 0b00000000;
+            res_index = 0;
+            res_value = 16384;
+            break;
+        case 1: //+- 4
+            resolution = 0b00001000;
+            res_index = 1;
+            res_value = 8192;
+            break;
+        case 2: //+- 8
+            resolution = 0b00010000;
+            res_index = 2;
+            res_value = 4096;
+            break;
+        case 3: //+- 16
+            resolution = 0b00011000;
+            res_index = 3;
+            res_value = 2048;
+            break;
+    }
+    i2c_write_reg(mpu6050_reg.address,mpu6050_reg.acc_config ,resolution);
+    accel_res = res_index;
+    accel_res_val = res_value;
+}
+
 static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
     uint8_t buffer[6];
 
@@ -89,14 +195,6 @@ int main()
     gpio_pull_up(I2C_SCL);
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
     mpu6050_reset();
-
-    //mpu state
-    uint8_t accel_res; uint16_t accel_res_val;                      // 0=> 16384, 1=>8192, 2=>4096, 3=>2048  
-    uint8_t gyro_res;                                               // 0=> 131,   1=>65.5, 2=>32.8, 3=>16.4 
-    int16_t accel_x_deviation, accel_y_deviation, accel_z_deviation;// accelerometer standard deviation
-    int16_t gyro_x_deviation, gyro_y_deviation, gyro_z_deviation;   // gyroscope standard deviation
-    int16_t accel_x_offset, accel_y_offset, accel_z_offset;         // accelerometer offset
-    int16_t gyro_x_offset, gyro_y_offset, gyro_z_offset;            // gyroscope offset
 
     //mpu data
     int16_t accel_raw[3];               // RAW X - Y - Z Acceleration
